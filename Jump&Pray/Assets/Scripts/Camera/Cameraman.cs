45f43fb6704d15e3dd65c865a9818e4db76b3772
Juan Digilio <juanignaciodigilio@gmail.com>
2025-04-29 00:39:23 -0300

    Update Cameraman.cs

--- Jump&Pray/Assets/Scripts/Camera/Cameraman.cs
+++ Jump&Pray/Assets/Scripts/Camera/Cameraman.cs
@@ -6,6 +6,7 @@ public class Cameraman : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
 
+    private float inputRotation;
     private float cameraRotation;
 
 
@@ -19,27 +20,56 @@ public class Cameraman : MonoBehaviour
         UpdatePosition();
     }
 
-    private void InitCamera()
-    {
-        mainCamera = Camera.main.GetComponent<Camera>();
-
-        mainCamera.transform.position = target.position + offset;
-        mainCamera.transform.LookAt(target);
-    }
-
     private void UpdatePosition()
     {
-        Quaternion newRotation = Quaternion.Euler(0, cameraRotation, 0);
-        Vector3 rotatedOffset = newRotation * offset;
-        rotatedOffset += target.position;
-
-        mainCamera.transform.position = rotatedOffset;
+        mainCamera.transform.position = CalculateNewPosition();
 
         mainCamera.transform.LookAt(target);
     }
 
     public void UpdateInputRotation(Vector2 input)
     {
+        inputRotation = input.x;
 
+        Debug.Log(inputRotation);
+    }
+
+    private void NormalizeRotation()
+    {
+        cameraRotation += inputRotation;
+
+        if (cameraRotation > 360)
+        {
+            cameraRotation -= 360;
+        }
+        else if (cameraRotation < 0)
+        {
+            cameraRotation += 360;
+        }
+    }
+
+    private Vector3 CalculateNewPosition()
+    {
+        NormalizeRotation();
+
+        Quaternion newRotation = Quaternion.Euler(0, cameraRotation, 0);
+
+        Vector3 rotatedOffset = newRotation * offset;
+        rotatedOffset += target.position;
+
+        return rotatedOffset;
+    }
+
+    private void InitCamera()
+    {
+        mainCamera = Camera.main;
+
+        if (mainCamera == null)
+        {
+            Debug.LogError("Main Camera not found!");
+        }
+
+        mainCamera.transform.position = target.position + offset;
+        mainCamera.transform.LookAt(target);
     }
 }
