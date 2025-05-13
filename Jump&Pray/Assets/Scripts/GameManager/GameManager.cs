0a858b693ec71449fddfb5f340b282f89ee5ee9a
Juan Digilio <juanignaciodigilio@gmail.com>
2025-05-13 11:45:21 -0300

    Added GameManager

---
+++ Jump&Pray/Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private Cameraman cameraman;
+    private PlayerController playerController;
+    private InputManager inputManager;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void RegisterCameraman(Cameraman cameraman)
+    {
+        this.cameraman = cameraman;
+    }
+
+    public void RegisterPlayer(PlayerController playerController)
+    {
+        this.playerController = playerController;
+    }
+
+    public void RegisterInputManager(InputManager inputManager)
+    {
+        this.inputManager = inputManager;
+    }
+
+    public Cameraman GetCameraman()
+    {
+        return cameraman;
+    }
+
+    public PlayerController GetPlayerController()
+    {
+        return playerController;
+    }
+
+    public InputManager GetInputManager()
+    {
+        return inputManager;
+    }
+}
